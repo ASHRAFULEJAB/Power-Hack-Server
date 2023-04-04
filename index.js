6623a9612545b1b329cab6dcf05888f759e79ec3
Ashraful Islam <19103189@iubat.edu>
2023-04-04 23:27:26 +0600

    done

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-const jwt =require('jsonwebtoken')
-require('dotenv').config()
+require("dotenv").config();
+
+//middleware
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-
 app.get("/", (req, res) => {
-  res.send("Power is runnning...");
+  res.send("Coding test is runnning...");
 });
+//  username= coreDevDB
+// password:STE7m7E60nlNHStd
+
+// mongoDB is connected Here
+const uri =
+  "mongodb+srv://coreDevDB:STE7m7E60nlNHStd@cluster0.dnw37y6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("coreDevDB").collection("users");
+    //email is posted here
+    app.post("/sendemail", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.listen(port, () => {
-  console.log(`Power Hack is running on ${port}`);
+  console.log(`Coding test is running on ${port}`);
 });
